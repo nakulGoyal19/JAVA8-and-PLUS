da6a0e883cbf54443b33052be165cbbf63a8d94b
nakulGoyal19 <nakul19goyal@gmail.com>
2020-08-08 16:19:23 +0530

    Lambda exp. with runnable thread.

---
+++ lambda-expressions/src/main/java/lambda/expression/ThreadLE.java
@@ -0,0 +1,31 @@
+package lambda.expression;
+
+/**
+ * @author nakulgoyal
+ *         08/08/20
+ **/
+public class ThreadLE {
+    public static void main(String[] args) {
+        /** Method : 1 */
+//        new Thread(new CodeToRun()).start();
+    
+        /** Method : 2 */
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the runnable inner method.");
+//            }
+//        }).start();
+    
+        /** Method : 3 Using lambda-expression */
+        new Thread(() -> System.out.println("Printing by using lambda exp.")).start();
+    }
+}
+
+//class CodeToRun implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("Printing from the runnable class method.");
+//    }
+//}
