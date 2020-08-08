11444b39d9e6f51a791448ee4ad5d12952147dbe
nakulGoyal19 <nakul19goyal@gmail.com>
2020-08-08 17:47:59 +0530

    Button FX - lambda exp.

---
+++ ButtonFX(lambda)/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    
+    @FXML
+    private Button clickMeButton;
+    
+    public void initialize() {
+//        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hey!! You just clicked me.");
+//            }
+//        });
+        clickMeButton.setOnAction(event -> System.out.println("Hey lambda!! You just clicked me."));
+    }
+}
