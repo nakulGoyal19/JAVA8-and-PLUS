9e81a89f8014473d2c0b7fbf4654ad44ab0bd081
nakulGoyal19 <nakul19goyal@gmail.com>
2020-08-09 13:06:51 +0530

    Int predicates

---
+++ predicates-and-suppliers/src/main/java/predicates/int_predicate/ConditionalPrint.java
@@ -0,0 +1,28 @@
+package main.java.predicates.int_predicate;
+
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+
+/**
+ * @author nakulgoyal
+ *         09/08/20
+ **/
+public class ConditionalPrint {
+    public static void main(String[] args) {
+        IntPredicate greaterThan100 = (val) -> val > 100;
+        IntPredicate lessThan200 = (val) -> val < 200;
+        
+        System.out.println(greaterThan100.test(40));  //false
+        System.out.println(greaterThan100.test(140));  //true
+        System.out.println(greaterThan100.and(lessThan200).test(140));  //true
+        System.out.println(greaterThan100.or(lessThan200).test(300));  //true
+        System.out.println(greaterThan100.negate().test(110));  //false
+        
+        //  Same for LongPredicate
+        LongPredicate greaterThan100L = (val) -> val > 100L;
+        System.out.println(greaterThan100L.test(200L));  //true
+    
+    }
+}
+
+
